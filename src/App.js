0ba885d9fa8abb26e53faacb6073b898da7b124d
joe lazarz <jlazarz87@gmail.com>
2019-09-19 09:52:16 -0400

    init setup of redux

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from 'react'
+import { Provider } from 'react-redux'
+import store from './store'
 
 import 'materialize-css/dist/css/materialize.min.css'
-import M from 'materialize-css/dist/js/materialize/min.js'
+import M from 'materialize-css/dist/js/materialize.min.js'
 import './App.css';
 
 const App = () => {
   
   useEffect(() => {
+    //Init Materialize JS
     M.AutoInit();
   })
 
   return (
-    <>
-    </>
+    <Provider store={store}>
+    </Provider>
   );
 }
 
