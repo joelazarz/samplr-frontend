cbbe7460ae2f2b82b8e327c0bc3364c75aa30145
joe lazarz <jlazarz87@gmail.com>
2019-09-19 09:19:35 -0400

    install materialize css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import 'materialize-css/dist/css/materialize.min.css'
+import M from 'materialize-css/dist/js/materialize/min.js'
 import './App.css';
 
-function App() {
+const App = () => {
+  
+  useEffect(() => {
+    M.AutoInit();
+  })
+
   return (
     <>
     </>
