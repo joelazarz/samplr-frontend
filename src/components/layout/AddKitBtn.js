60f783c38f246ae15385a719d3fca55aab0a900f
joe lazarz <jlazarz87@gmail.com>
2019-09-24 09:55:46 -0400

    discovery page clean up

--- src/components/layout/AddKitBtn.js
+++ src/components/layout/AddKitBtn.js
@@ -4,7 +4,7 @@ const AddKitBtn = () => {
 
     return (
         <div className='fixed-action-btn'>
-            <a href="#add-kit-modal" className="btn-floating btn-large black darken-2 modal-trigger">
+            <a href="#add-kit-modal" className="btn-floating btn-medium black darken-2 modal-trigger">
             <i className="large material-icons">add </i></a>
             <h6>Add Kit</h6>
         </div>
