ca0dae9dd2f8de08f0948402743f322cd79563a2
joe lazarz <jlazarz87@gmail.com>
2019-09-27 16:07:52 -0400

    add notes set up with redux

--- src/components/waveform/NoteForm.js
+++ src/components/waveform/NoteForm.js
@@ -1,8 +1,15 @@
 import React, { useState } from 'react'
+import { connect } from 'react-redux';
+import { addNote } from '../../actions/noteActions';
 import M from 'materialize-css/dist/js/materialize.min.js'
 
-const NoteForm = ({ kitId, noteStart, noteEnd, noteSubmit }) => {
+const NoteForm = ({ kitId, noteStart, noteEnd, noteSubmit, addNote }) => {
     const [note, setNote] = useState('')
+
+    let kit_id = kitId
+    let note_pad_start = noteStart
+    let note_pad_end = noteEnd
+    let user_id = 1
 
 
     const onSubmit = () => {
@@ -10,19 +17,19 @@ const NoteForm = ({ kitId, noteStart, noteEnd, noteSubmit }) => {
         if(note === '' ){
             M.toast({ html: 'Please enter note...' })
         } else {
-            const newKit = {
-                // note,
-                // detail,
-                // image,
-                // sample
+            const newNote = {
+                kit_id,
+                user_id,
+                note,
+                note_pad_start,
+                note_pad_end
             }
-            // addKit(newKit)
-            M.toast({ html: 'Kit Added'})
+            console.log('NEWNOTE FROM FORM:', newNote)
+            addNote(newNote)
+            M.toast({ html: 'Note Added'})
         }
-        // setName('')
-        // setDetail('')
-        // setImage('')
-        // setSample('')
+        setNote('')
+
     }
 
 
@@ -30,7 +37,11 @@ const NoteForm = ({ kitId, noteStart, noteEnd, noteSubmit }) => {
         <>
             <div className="input-field col s6">
             <i className="material-icons prefix">mode_edit</i>
-            <input id="icon_prefix2" className="materialize-input" placeholder="Leave note..."></input>
+            <input 
+            id="icon_prefix2" 
+            className="materialize-input" 
+            onChange={e => setNote(e.target.value)}
+            placeholder="Leave note..."></input>
             <label htmlFor="icon_prefix2"></label>
             </div>
             <div>
@@ -43,4 +54,4 @@ const NoteForm = ({ kitId, noteStart, noteEnd, noteSubmit }) => {
     )
 }
 
-export default NoteForm
+export default connect(null, { addNote })(NoteForm)
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import discoveryReducer from './discoveryReducer'
 import kitReducer from './kitReducer'
+import noteReducer from './noteReducer'
 import digReducer from './digReducer'
 
 export default combineReducers({
     kits: discoveryReducer,
     kit: kitReducer,
-    digs: digReducer
+    digs: digReducer,
+    notes: noteReducer
 })
