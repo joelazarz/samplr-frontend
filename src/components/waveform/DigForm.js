d54189b894574cdf9ea335245c227113cf29d049
joe lazarz <jlazarz87@gmail.com>
2019-09-27 11:10:17 -0400

    conditional rendering of dig and note form

---
+++ src/components/waveform/DigForm.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const DigForm = () => {
+    return (
+        <>
+            <div className="input-field col s6">
+            <i className="material-icons prefix">mode_edit</i>
+            <input id="icon_prefix2" className="materialize-input" placeholder="Set Region Memory..."></input>
+            <label htmlFor="icon_prefix2"></label>
+            </div>
+        </>
+    )
+}
+
+export default DigForm
