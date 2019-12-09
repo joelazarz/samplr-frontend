e719e07b4ebe71b75e083f2cc029b3aa8ee1326f
joe lazarz <jlazarz87@gmail.com>
2019-12-09 13:05:28 -0500

    starting landing page guide

--- src/components/open/Guide.js
+++ src/components/open/Guide.js
@@ -2,8 +2,24 @@ import React from 'react'
 
 const Guide = () => {
     return (
-        <div>
-            
+        <div className="landing-guide">
+        Welcome to Sampler(), a social and interactive digital audio workstation. Sampler() allows you to upload audio files, save regions and notes within the audio’s waveform, trigger region playback in real-time and adjust playback speed/pitch while you play.
+        <br></br>
+        <br></br>
+        Please sign-up or login(if you’ve already signed up). You will automatically be redirected to the discovery page, where you’ll be able to browse all “kits” uploaded by our users. Your password will NEVER be accessible to the developers or other users. Auto-login is made possible by saving an encrypted token in the local storage of your browser. If you have any questions or concerns, please email josephlazarz@gmail.com. 
+        <br></br>
+        <br></br>
+        Once you load up an individual kit, you’ll be able to load in regions, drag them around the waveform, adjust their length and begin triggering them with keyboard commands, which can be found in the Guide (lower right-hand side on each kit). 
+        <br></br>
+        <br></br>
+        Not only can you improvise with your own custom regions, you’ll be able to save regions, making them accessible to other users. These will appear on the right hand side of your screen. Loading saved regions into the waveform is easy, just cue them with the + button, then, underneath the region space you want to use, press “load”.
+        <br></br>
+        <br></br>
+        Using the memory region in the toolbar below the waveform, you can save sections of the waveform for future use while also making it available to other users. 
+        <br></br>
+        <br></br>
+        Sampler() is currently in its earliest beta version, please explore the possibilities and if you come across a problem, or have any suggestions, please email josephlazarz@gmail.com. Thank you so much for being a part of this, your participation helps to grow this platform into resource with hopes to lower barriers of entry into DJing, music production and the art of sampling. 
+
         </div>
     )
 }
--- src/components/open/LandingPage.js
+++ src/components/open/LandingPage.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import Login from './Login';
 import Signup from './Signup';
+import Guide from './Guide';
 
 
 const LandingPage = ({ setSessionUser, user }) => {
@@ -18,7 +19,7 @@ const LandingPage = ({ setSessionUser, user }) => {
                 <span className='graphic-dot'>.</span>sampler<span className='graphic-par'>()</span>
                 </div>
                 </div>
-                    {user ? <h4>Welcome {user.username}</h4> 
+                    {user ? <h5>Welcome {user.username}...continue to <Link to ="/discovery">Discovery</Link></h5>
                     : 
                     <>
                     {login ? <Signup setSessionUser={setSessionUser} />
@@ -33,7 +34,9 @@ const LandingPage = ({ setSessionUser, user }) => {
             <div className="col s8 blue-grey darken-4">
             <div className="height">
                 <div className="container">
-                    <Link to ="/discovery">Discovery</Link>
+                <div className="guide-container">
+                    <Guide />
+                </div>
                 </div>
             </div>
             </div>
