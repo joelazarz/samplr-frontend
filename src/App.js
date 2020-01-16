edf94de87865d288d6d1248e58cff491a734b04c
joe lazarz <jlazarz87@gmail.com>
2020-01-16 14:28:43 -0500

    check for light/dark mode on user

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ const App = () => {
   };
 
   const setSessionUser = (sessionUser) => {
-    // sessionUser.user.darkmode || null ? setNightMode(true) : setNightMode(false)
+    sessionUser.user.darkmode || null ? setNightMode(true) : setNightMode(false)
     setUser(sessionUser.user)
   }
 
