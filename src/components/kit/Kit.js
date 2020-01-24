8e2a3e7aa69a3faee8f77f30baf0fb54cecdfa1a
joe lazarz <jlazarz87@gmail.com>
2020-01-24 12:03:00 -0500

    LoopStation config - about finished

--- src/components/kit/Kit.js
+++ src/components/kit/Kit.js
@@ -15,6 +15,7 @@ const Kit = ({ kit: { kit, loading }, user, getKit, nightMode }) => {
 
     const [notesButton, setNotesButton] = useState(false)
     const [memoryButton, setMemoryButton] = useState(true)
+    const [detailButton, setDetailButton] = useState(false)
     const [digCue, setDigCue] = useState([])
 
     useEffect(() => {
@@ -27,9 +28,16 @@ const Kit = ({ kit: { kit, loading }, user, getKit, nightMode }) => {
     const viewNotesButton = () => {
         setNotesButton(true)
         setMemoryButton(false)
+        setDetailButton(false)
     }
     const viewMemoryButton = () => {
         setMemoryButton(true)
+        setNotesButton(false)
+        setDetailButton(false)
+    }
+    const viewDetailButton = () => {
+        setDetailButton(true)
+        setMemoryButton(false)
         setNotesButton(false)
     }
     const cueMemoryButton = (dig) => {
@@ -52,43 +60,32 @@ const Kit = ({ kit: { kit, loading }, user, getKit, nightMode }) => {
         <>
         <div className="row">
             <div className="col s8" style={nightMode ? theme.dmPrimary : theme.lmWhite}> 
-            <div className="kit-main-container">
-                <div className="kit-waveform-container">
-                    <Waveform nightMode={nightMode} key={kit.id} userId={user.id} id={kit.id} src={kit.sample} notes={kit.notes} digs={kit.digs} digCue={digCue} shiftDig={shiftDig} />
-                </div>
-
-                {/* <div className="kit-name-container" style={nightMode ? theme.dmAccent : theme.lmLightBlue}>
-                    <span>[ Kit Name ] : {kit.name}</span>
-                </div> */}
-
-                {/* <div className= "kit-detail">
-                    <div className="kit-detail-container" style={nightMode ? theme.dmSecondary : theme.lmAccentTwo}>
-                        <div className="kit-detail-text-container" style={nightMode ? theme.dmKitDetail : theme.lmAccent}>
-                        {kit.detail}
-                        </div>
+                <div className="kit-main-container">
+                    <div className="kit-waveform-container">
+                        <Waveform nightMode={nightMode} key={kit.id} userId={user.id} id={kit.id} src={kit.sample} notes={kit.notes} digs={kit.digs} digCue={digCue} shiftDig={shiftDig} />
                     </div>
 
-                    <div className="kit-detail-image-container" style={nightMode ? theme.dmImgContainer : theme.lmGrey}>
-                        <img src={kit.image} alt={kit.name} />
-                    </div>
-                </div> */}
-                
                 </div>
             </div>
-
 
             <div className="col s4" style={nightMode ? theme.dmSecondary : theme.lmAccentTwo}>
             <div className="height">
 
+            <div className="kit-name-container" style={nightMode ? theme.dmAccent : theme.lmLightBlue}>
+                <span>[ Kit ] : {kit.name}</span>
+            </div>
+
             <div className="notes-specs" style={nightMode ? theme.dmAccent : theme.lmWhite}>
                     <div className="specs-header" style={nightMode ? theme.dmSpecHeader : theme.lmSpecHeader}>
-                    <div className='specs-button'><button style={nightMode ? theme.dmBtnSidebar : theme.lmBtnSidebar} onClick={viewNotesButton} className="waves-effect waves-light btn-small">Notes</button></div>
                     <div className='specs-button'><button style={nightMode ? theme.dmBtnSidebar : theme.lmBtnSidebar} onClick={viewMemoryButton} className="waves-effect waves-light btn-small">Memory</button></div>
+                    <div className='specs-button'><button style={nightMode ? theme.dmBtnSidebar : theme.lmBtnSidebar} onClick={viewNotesButton} className="waves-effect waves-light btn-small">Notes</button></div>
+                    <div className='specs-button'><button style={nightMode ? theme.dmBtnSidebar : theme.lmBtnSidebar} onClick={viewDetailButton} className="waves-effect waves-light btn-small">Detail</button></div>
                     </div>
                     <>
                     <div className='spec-content'>
                     { notesButton ? kit.notes.map(note => <NotesCard nightMode={nightMode} key={note.id} note={note} />) : <></> }
                     { memoryButton ? kit.digs.map(dig => <MemoryCard nightMode={nightMode} clickHandler={cueMemoryButton} key={dig.id} dig={dig} />) : <></> }
+                    { detailButton ? <><div style={{textAlign: 'center', width: '100%', marginBottom: '1em'}}><img style={{width: '50%'}} src={kit.image} alt={kit.name} /></div><span style={{padding: '1em'}}>{kit.detail}</span></> : <></> }
                     </div>
                     </>
             </div>
