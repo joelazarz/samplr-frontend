f7b08c47accb9e165417ef243521d89ad582581c
joe lazarz <jlazarz87@gmail.com>
2020-01-24 17:04:38 -0500

    need to fix sign up

--- src/components/open/DemoInstructions.js
+++ src/components/open/DemoInstructions.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const DemoInstructions = () => {
   return (
     <div className="demo-instructions">
-      <span style={{fontSize: '0.9em'}}>If you are here to demo or would rather not sign-up, please sign in with the following:</span>
+      <span style={{fontSize: '0.9em'}}>If you are here to demo this application please sign in with the following:</span>
       <br/>
       <br/>
       <span style={{fontWeight: 'bold'}}>Username:</span><span> demo</span>
